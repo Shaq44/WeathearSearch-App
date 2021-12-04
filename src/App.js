777cf8a6c4555e630547603e2ac616d5259e31c9
Shaq 44 <Shaquan44@gmail.com>
2021-12-04 12:15:02 -0600

    WeatherSearch App

--- src/App.js
+++ src/App.js
@@ -7,40 +7,51 @@ import Search from './Components/Search';
 const App = () => {
   const [header]= useState('Weather App');
   const[weatherInfo,setWeatherData] = useState([]);
+  const [cityName,setCityName] = useState('');
+  
   
 
   useEffect( () =>{
-    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=466b5c9748efb6f0d78ce0ea606ef8a1`;
-    fetch(weatherUrl)
-    .then(response => response.json())
-    .then(weatherData =>{ 
-      console.log(weatherData);
-      setWeatherData(weatherData)
-    })
-    .catch(err => console.log('err:',err))
+    
+      function getWeatherData (cityName){
+          
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=466b5c9748efb6f0d78ce0ea606ef8a1`;
+      fetch(weatherUrl)
+      .then(response => response.json())
+      .then(weatherData =>{ 
+        console.log(weatherData);
+        setWeatherData(weatherData)
+      })
+      .catch(err => console.log('err:',err))
+    }
+    getWeatherData();
+    
+    console.log("useEffect ran...");
+     
 
    
   
 },[])
 
+
+
+
+
 console.log(weatherInfo.name);
 console.log(weatherInfo.weather);
-weatherInfo.weather.map((data)=>{
+/*weatherInfo.weather.map((data)=>{
   console.log(data.description);
-})
+})*/
   return (
     <div className="App">
       <header className="App-header row">
      <h1 id="header">{header} </h1>   
       </header>
 
-      <Search />
+      <Search setCityName={cityName} />
      <div>
      </div>
      <h1>{weatherInfo.name}</h1>
-     {weatherInfo.weather.map((info)=>(
-       <h3>{info.description}</h3>
-     ))}
      
     </div>
   );
