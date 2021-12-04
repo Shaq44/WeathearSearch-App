427cb6347cfb31dded4a298563fe4e50f98dec48
Shaq 44 <Shaquan44@gmail.com>
2021-12-03 23:02:37 -0600

    WeatherSearch App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React, { useEffect, useState } from 'react';
+import Search from './Components/Search';
+
+
+const App = () => {
+  const [header]= useState('Weather App');
+  const[weatherInfo,setWeatherData] = useState([]);
+  
+
+  useEffect( () =>{
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=466b5c9748efb6f0d78ce0ea606ef8a1`;
+    fetch(weatherUrl)
+    .then(response => response.json())
+    .then(weatherData =>{ 
+      console.log(weatherData);
+      setWeatherData(weatherData)
+    })
+    .catch(err => console.log('err:',err))
+
+   
+  
+},[])
+
+console.log(weatherInfo.name);
+console.log(weatherInfo.weather);
+weatherInfo.weather.map((data)=>{
+  console.log(data.description);
+})
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header row">
+     <h1 id="header">{header} </h1>   
       </header>
+
+      <Search />
+     <div>
+     </div>
+     <h1>{weatherInfo.name}</h1>
+     {weatherInfo.weather.map((info)=>(
+       <h3>{info.description}</h3>
+     ))}
+     
     </div>
   );
 }
 
+
 export default App;
+
---
+++ src/Components/Search.js
@@ -0,0 +1,24 @@
+import React,{useState} from 'react';
+
+const Search = () =>{
+    const[cityName,setCityName] = useState('');
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+        alert(`Submitting name ${cityName}`)
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <label>
+                Search City:
+                <input type='text' value={cityName} onChange={e => setCityName(e.target.value)}/>
+                
+            </label>
+            <input type="submit" value="Submit"/>
+        </form>
+    )
+
+}
+
+export default Search;
